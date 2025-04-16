cd6642785b6aa689ba59a672b13a916270b8fdf8
butterflyshi <s111219006@stu.ntue.edu.tw>
2025-04-16 22:59:38 +0800

    回宿舍前的首頁更新

--- src/components/Dbm.jsx
+++ src/components/Dbm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Dbm() {
   return (
-    <div>
+    <div className="content">
       <h1 className="text-3xl font-bold">首頁</h1>
       <p>歡迎來到我們的網站！</p>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer/index";
 import Dbm from "../components/Dbm";
 
 function Home() {
 
     return (
-      <div className="container mx-auto main-layout bg-[#FFF9F0] min-h-screen">
-        <Header 
-          title="不要B我市集"
+      <div className="container mx-auto main-layout bg-[#FFF9F0] min-h-screen flex flex-col items-center">
+        <div className="flex flex-col items-center">
+        <Navbar />
+        <img
+        src="/images/視覺圖_light.svg"
+        alt="不要B我市集"
+        className="w-full h-auto"
         />
+        </div>
+        
         <Dbm className="content" />
         <Footer className="footer" />
       </div>
