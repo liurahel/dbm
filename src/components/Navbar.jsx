cf776aff17a227dce3733085aea48ab9f7e73b67
butterflyshi <s111219006@stu.ntue.edu.tw>
2025-04-16 20:47:07 +0800

    圖片路徑修改

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function NavBar() {
   return (
     <div className="flex items-center h-20 w-6xl fixed top-8 z-50 bg-[#FFF9F0] px-4 py-4 shadow-md rounded-[20px]">      
         <div className="flex items-center justify-center max-w-6xl mx-auto">
-        <img src="public/images/企劃書.png" alt="不要B我LOGO" className="w-30 h-auto absolute item-center left-2 md:left-6 cursor-pointer" />
+        <img src="/images/企劃書.png" alt="不要B我LOGO" className="w-30 h-auto absolute item-center left-2 md:left-6 cursor-pointer" />
             {/* 左側選單連結 */}
             <div className="flex space-x-6">
                 {navBarContent.map(({ to, label }) => (
