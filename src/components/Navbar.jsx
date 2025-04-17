e66418d3c944f69ba3cfb4c2a8481b17afb900de
620 <s111219035@stu.ntue.edu.tw>
2025-04-18 03:36:14 +0800

    調一下HAMBERGER

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
         to={to}
         onClick={() => setIsOpen(false)}
         className={({ isActive }) =>
-            `mx-6 bg-[#FBCD5C] text-base font-semibold transition-all duration-500 ease-in-out ${
+            `mx-6 text-base font-semibold transition-all duration-500 ease-in-out ${
             isActive ? "opacity-100 font-normal" : "opacity-70"
             } hover:opacity-100 hover:[text-shadow:0px_0px_30px_white`
         }
@@ -34,16 +34,16 @@ export default function NavBar() {
   return (
     <>
     {/* 桌面版導覽列 */}
-    <div className="hidden md:flex justify-aroundflex items-center h-20 w-6xl fixed top-8 z-50 bg-[#FFF9F0] px-4 py-4 shadow-md rounded-[20px]">
+    <div className="hidden md:flex justify-aroundflex items-center h-20 w-6xl fixed top-8 z-50 bg-[#FFF9F0] px-4 py-4 shadow-md rounded-[20px] text-[#854D8E]">
       <div className="flex items-center justify-center max-w-6xl mx-auto">
         <Link to="/">
         <img src="/images/企劃書.png" alt="不要B我LOGO" className="w-30 h-auto absolute item-center left-2 md:left-6 top-1.5 cursor-pointer" /></Link>
         <NavBarContent />
         {/* 右側購物車 */}
-          <div className="flex items-center text-[#854D8E]">
+          <div className="flex items-center">
             <CartSummary />
           </div>
-          <div className="flex items-center text-[#854D8E]">
+          <div className="flex items-center">
             <SetColorMode/>
           </div>
       </div>
@@ -61,12 +61,15 @@ export default function NavBar() {
         <div className="drawer-side z-9999">
           <label htmlFor="drawer-toggle" className="drawer-overlay" onClick=
           {() => setIsOpen(false)}></label>
-          <div className="menu p-4 w-64 min-h-full bg-[#FBCD5C] text-[#854D8E] text-center">
+          <div className="menu p-4 w-64 min-h-full bg-[#FBCD5C] text-[#854D8E] text-center items-center">
             <h2 className="text-xl font-bold mb-10">不要B我市集 | 選單</h2>
             <NavBarContent />
+            <Link to="/" className="mt-auto">
+              <img src="/images/企劃書.png" alt="不要B我LOGO" className="w-40 md:w-48 h-auto cursor-pointer" />
+            </Link>
           </div>
         </div>
-        <div className="fixed top-4 right-4 flex items-center space-x-4 z-50">
+        <div className="fixed top-4 right-4 flex items-center space-x-4 z-50 justify-center">
           <div className="w-10 h-10 flex items-center justify-center text-[#FBCD5C]">
             <CartSummary />
           </div>
