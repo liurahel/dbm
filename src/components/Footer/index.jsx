9d31b9bedfb3a553fa46c18f984bfa51b29cf8b3
butterflyshi <s111219006@stu.ntue.edu.tw>
2025-06-05 06:02:26 +0800

    改夫特

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,33 +9,33 @@ export default function Footer() {
   return (
     <div>
       <footer className="footer footer-bg footer-text py-6 w-full">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-screen-xl mx-auto px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 pr-40 sm:gap-6 sm:p-0 max-w-screen-xl mx-auto">{/*sm:px-4 pb-5 border-b-1*/}
           <div>
-            <h4 className="font-bold mb-2">不要B我市集</h4>
-              <p className="text-xs sm:text-sm m-0">數位二甲 111219036 施亞蝶<br></br>數位二甲 111219035 劉恩伶<br></br>113學年第二學期<br></br>跨平台前端工程設計 期末專題</p>
+            <h4 className="font-bold mb-2 text-sm sm:text-base">不要B我市集</h4>
+              <p className="text-xs sm:text-sm m-0 leading-relaxed">數位二甲 111219036 施亞蝶<br></br>數位二甲 111219035 劉恩伶<br></br>113學年第二學期<br></br>跨平台前端工程設計 期末專題</p>
           </div>
           <div>
-            <h4 className="font-bold mb-2">攤位相關</h4>
-              <p className="text-xs sm:text-sm m-0">報名方式<br></br>注意事項<br></br>收費制度<br></br>攤位地圖</p>
+            <h4 className="font-bold mb-2 text-sm sm:text-base">攤位相關</h4>
+              <p className="text-xs sm:text-sm m-0 leading-relaxed">報名方式<br></br>注意事項<br></br>收費制度<br></br>攤位地圖</p>
           </div>
           <div>
-            <h4 className="font-bold mb-2">常見問題</h4>
-              <p className="text-xs sm:text-sm m-0">訊息公告<br></br>退貨政策<br></br>訂單查詢<br></br>服務條款</p>
+            <h4 className="font-bold mb-2 text-sm sm:text-base">常見問題</h4>
+              <p className="text-xs sm:text-sm m-0 leading-relaxed">訊息公告<br></br>退貨政策<br></br>訂單查詢<br></br>服務條款</p>
           </div>
           <div>
-            <h4 className="font-bold mb-2">聯絡資訊</h4>
+            <h4 className="font-bold mb-2 text-sm sm:text-base">聯絡資訊</h4>
             <div className="sm:mr-auto mb-4">
-              <div className="space-x-2">
+              <div className="group items-center space-x-2 cursor-pointer">
                 <FontAwesomeIcon icon={faPhone} className="hover:rotate-45" />
                 <span className="text-xs sm:text-sm group-hover:text-[#f8eacc]">+886-2-1234-1234</span>
               </div>
-              <div className="h-1 sm:h-2"></div>
+              <div className="h-0 sm:h-0.5"></div>
               <div className="group inline-flex items-center space-x-2 cursor-pointer">
                 <FontAwesomeIcon icon={faEnvelope} className="hover:rotate-45" />
                 <span className="text-xs sm:text-sm group-hover:text-[#f8eacc]">s111219999@stu.ntue.edu.tw</span>
               </div>
             </div>
-            <h4 className="font-bold mb-2">社群媒體</h4>
+            <h4 className="font-bold mb-2 text-sm sm:text-base">社群媒體</h4>
             <div className="flex space-x-6 text-3xl">
               <FontAwesomeIcon icon={faFacebook} className=" hover:text-[#FFF9F0] 
               transition-all duration-200 transform hover:scale-125" />
@@ -47,7 +47,7 @@ export default function Footer() {
         {/* 版權 */}
         <div className="w-full px-4">
           <div className="max-w-5xl mx-auto text-center">
-            <hr className="my-2 mt-5 w-xs sm:w-2xl sm:my-4 icon-text mx-auto hover:text-[#086776]" />
+            <hr className="my-2 mt-5 w-xs sm:w-5xl sm:my-4 icon-text mx-auto" /> 
             <span className="text-xs sm:text-sm group">
               ©不要
               <span className="inline-block group-hover:scale-125 group-hover:tracking-widest transition-transform duration-200">B</span>
